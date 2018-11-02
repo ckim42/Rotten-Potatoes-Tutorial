b60154e8247af6654c0082101f0e09da7bc12f1f
ilikecoffee18 <cherish.kim@students.makeschool.com>
2018-11-02 00:49:27 -0700

    Updated root to use review data from mongodb

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 
 // mock array of projects
-let reviews = [
-  { title: "Great Review", movieTitle: "Batman II" },
-  { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+// let reviews = [
+//   { title: "Great Review", movieTitle: "Batman II" },
+//   { title: "Awesome Movie", movieTitle: "Titanic" }
+// ]
+
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -15,8 +17,19 @@ app.set('view engine', 'handlebars');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 const Review = require('./models/review');
 
+// INDEX
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  // gets all reviews from the database
+  Review.find()
+    // promise that sends reviews data to the page
+    .then(reviews => {
+        // renders the page with the reviews data referred to as reviews
+        res.render('reviews-index', { reviews: reviews });
+    })
+    // error catch 
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.listen(3000, () => {
