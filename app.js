43a328708aff327b63f64ce8e2fc65c8fc9162ca
ilikecoffee18 <cherish.kim@students.makeschool.com>
2018-11-04 21:22:57 -0800

    Added MongoDB and Review model (retrying)

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
-let reviews = [
-  { title: "Review", movieTitle: "Batman II" },
-  { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+const Review = mongoose.model('Review', { //model is capitalized and singular, like with classes
+  title: String,
+  movieTitle: String
+});
+
+Review.find()
+  .then(review => {
+  })
+  .catch(err => {
+
+  });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => { //Index
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 });
 
 app.listen(3000, () => {
